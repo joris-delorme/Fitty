a8ee946cbb84b2ca6442cb22fa667efab35713ce
Joris Delorme <joris.delorme38@gmail.com>
2022-08-19 17:58:39 +0200

    url

--- hydrogen.config.js
+++ hydrogen.config.js
@@ -2,15 +2,14 @@ import {defineConfig, CookieSessionStorage} from '@shopify/hydrogen/config';
 
 export default defineConfig({
   shopify: () => ({
-    defaultCountryCode: 'US',
-    defaultLanguageCode: 'EN',
+    defaultCountryCode: 'FR',
+    defaultLanguageCode: 'FR',
     storeDomain:
       // @ts-ignore
-      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'fitty.shop',
+      Oxygen?.env?.SHOPIFY_STORE_DOMAIN || 'fittyfitness-9371.myshopify.com',
     storefrontToken:
       // @ts-ignore
-      Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN ||
-      '856c197b4e0e3c9783e1bdcf6b06f5de',
+      Oxygen?.env?.SHOPIFY_STOREFRONT_API_PUBLIC_TOKEN || '856c197b4e0e3c9783e1bdcf6b06f5de',
     storefrontApiVersion: '2022-07',
   }),
   session: CookieSessionStorage('__session', {
